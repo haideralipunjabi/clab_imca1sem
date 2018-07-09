6293220c9d2d68e1b57620ef663ccbd90dd4717f
Haider Ali Punjabi <haiderali@cyberservices.com>
2018-07-09 21:50:39 +0530

    9/7

--- vowel_or_consonant/main.c
+++ vowel_or_consonant/main.c
@@ -7,26 +7,28 @@ Date: 26-06-2018
 #include <stdio.h>
 #include <stdlib.h>
 
+int isVowel(char c);
 int main()
 {
     // Variable Deceleration
     char c;
-
     //Input
     printf("Input letter: ");
-    scanf("%c", &c);
+    scanf(" %c", &c);
     // Processing
-    if(c == 'a' || c == 'A')
-        printf("%c is a vowel", c);
-    else if(c == 'e' || c == 'E')
-        printf("%c is a vowel", c);
-    else if(c == 'i' || c == 'I')
-        printf("%c is a vowel", c);
-    else if(c == 'o' || c == 'O')
-        printf("%c is a vowel", c);
-    else if(c == 'u' || c == 'U')
-        printf("%c is a vowel", c);
-    else
-        printf("%c is a consonant", c);
+    if(isVowel(c)){
+      printf("%c is a vowel\n", c);
+    }
+    else{
+      printf("%c is a consonant\n", c);
+    }
     return 0;
+}
+/*
+ * function isVowel - checks whether a character is a vowel or not; args: char c - character to check; returns 1 or 0
+ */
+int isVowel(char c){
+  if(c == 'a' || c == 'A' || c == 'e' || c == 'E' || c == 'i' || c == 'I' || c == 'o' || c == 'O' || c == 'u' || c == 'U')
+    return 1;
+  return 0;
 }
