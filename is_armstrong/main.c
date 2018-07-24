63161220cbbb46c8fdb8e82ba56f0e798e48d03a
Haider Ali Punjabi <haiderali@cyberservices.com>
2018-07-24 20:21:03 +0530

    From Home

--- is_armstrong/main.c
+++ is_armstrong/main.c
@@ -7,6 +7,8 @@ Date: 09-07-2018
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <math.h>
+int isArmstrong(int);
 int main()
 {
     // Variable Deceleration
@@ -28,7 +30,7 @@ int isArmstrong(int num){
     int sum = 0, _num = num;
     // Processing
     while(num > 0){
-        sum += (int)round(pow(num % 10,3));
+        sum += (num%10)*(num%10)*(num%10);
         num /= 10;
     }
     return (sum==_num) ? 1 : 0;
